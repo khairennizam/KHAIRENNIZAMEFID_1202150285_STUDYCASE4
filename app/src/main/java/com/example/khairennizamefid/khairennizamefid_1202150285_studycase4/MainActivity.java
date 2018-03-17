1d59a28e5c038b8bccf8103058776aa21700e114
khairennizam <khairennizam@gmail.com>
2018-03-17 20:12:06 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/khairennizamefid/khairennizamefid_1202150285_studycase4/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.khairennizamefid.khairennizamefid_1202150285_studycase4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void tampilNama(View view) {
+        Intent i = new Intent(getApplicationContext(),Nama.class);
+        startActivity(i);
+    }
+
+    public void tampilGambar(View view) {
+        Intent i = new Intent(getApplicationContext(),Gambar.class);
+        startActivity(i);
+    }
+}
